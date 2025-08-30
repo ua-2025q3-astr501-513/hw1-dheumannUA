bda710a3e609d8832a6e82510de6869100bacd13
dheumannUA <dr.dirk.heumann@gmail.com>
2025-08-29 17:55:19 -0700

    Text for problem 2 inserted into p2.py file

---
+++ hw1/p2.py
@@ -0,0 +1,2 @@
+# Part 2: Integer Negation and Subtraction Using NAND Gates (1 point)
+# Objective:   Implement a function that performs integer negation using only NAND gates and use it to implement subtraction
